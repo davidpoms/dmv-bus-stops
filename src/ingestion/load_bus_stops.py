b371fc03649412a6c280684d45d56dcd94d2434b
davidpoms <dpoms@dcpca.org>
2026-07-24 00:20:01 -0400

    Update load_bus_stops.py

--- src/ingestion/load_bus_stops.py
+++ src/ingestion/load_bus_stops.py
@@ -17,7 +17,14 @@ downstream analysis.
 
 import json
 from pathlib import Path
+import sys
+from pathlib import Path
 
+ROOT_DIR = Path(__file__).resolve().parents[2]
+
+sys.path.append(
+    str(ROOT_DIR)
+)
 from config import BUS_STOP_FILE
 
 
