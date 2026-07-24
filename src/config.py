0d92e912525be9d8cba102a5d5474ae0467cdc21
davidpoms <dpoms@dcpca.org>
2026-07-24 00:50:58 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -81,7 +81,7 @@ MANIFEST_DIR = (
     f"{PROCESSED_DATA_DIR}/manifests"
 )
 
-
+BUS_STOP_API_URL = "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_Rail_Bus_WebMercator/MapServer/53/query?outFields=*&where=1%3D1&f=geojson"
 
 # ------------------------------------------------------------
 # Street View settings
